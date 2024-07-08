0e728a1ddb8a7369f47b17f31d3b701174de37be
lifeispotato <msw2381@naver.com>
2024-07-08 15:32:50 +0900

    fix: 페이지 나눔, localstoreage 적용

---
+++ src/pages/resultPage.jsx
@@ -0,0 +1,290 @@
+import { useEffect, useRef, useState } from "react";
+import PretendardText from "../components/pretendardText";
+import SizeBox from "../components/sizeBox";
+import StandardSizeBox from "../components/standardSizeBox";
+import { standardSize } from "../constant/size";
+import { useMediaQuery } from "react-responsive";
+import html2canvas from "html2canvas";
+
+function ResultPage() {
+  const divRef = useRef(null);
+  const isMobile = useMediaQuery({ query: "(max-width: 430px)" });
+
+  const [name, setName] = useState(null);
+  const [shoulder, setShoulder] = useState(null);
+  const [chest, setChest] = useState(null);
+  const [sleeve, setSleeve] = useState(null);
+  const [waist, setWaist] = useState(null);
+  const [top, setTop] = useState(null);
+  const [bottom, setBottom] = useState(null);
+  const [pattern, setPattern] = useState(null);
+
+  const [topSize, setTopSize] = useState("size0");
+  const [bottomSize, setBottomSize] = useState("size0");
+
+  const [scrollHeight, setScrollHeight] = useState(0);
+
+  useEffect(() => {
+    setName(localStorage.getItem("name"));
+    setShoulder(localStorage.getItem("shoulder"));
+    setChest(localStorage.getItem("chest"));
+    setSleeve(localStorage.getItem("sleeve"));
+    setWaist(localStorage.getItem("waist"));
+    setTop(localStorage.getItem("top"));
+    setBottom(localStorage.getItem("bottom"));
+    setPattern(localStorage.getItem("pattern"));
+    setTopSize(localStorage.getItem("topSize"));
+    setBottomSize(localStorage.getItem("bottomSize"));
+  }, []);
+
+  useEffect(() => {
+    const updateScrollHeight = () => {
+      if (divRef.current) {
+        const newScrollHeight = divRef.current.scrollHeight;
+        setScrollHeight(newScrollHeight);
+        console.log("Scroll Height:", newScrollHeight);
+
+        let message = { height: newScrollHeight };
+        window.top.postMessage(message, "*");
+      }
+    };
+
+    updateScrollHeight();
+
+    // MutationObserver 설정
+    const observer = new MutationObserver(updateScrollHeight);
+    if (divRef.current) {
+      observer.observe(divRef.current, {
+        childList: true,
+        subtree: true,
+        characterData: true,
+      });
+    }
+
+    // 윈도우 리사이즈 이벤트 추가
+    window.addEventListener("resize", updateScrollHeight);
+
+    // 클린업 함수
+    return () => {
+      observer.disconnect();
+      window.removeEventListener("resize", updateScrollHeight);
+    };
+  }, []); // 빈 배열로 설정하여 컴포넌트가 마운트될 때 한 번만 실행
+
+  const onClickDownloadButton = () => {
+    const target = document.getElementById("download");
+    if (!target) {
+      return alert("사진 저장에 실패했습니다.");
+    }
+    html2canvas(target).then((canvas) => {
+      const link = document.createElement("a");
+      document.body.appendChild(link);
+      link.href = canvas.toDataURL("image/png");
+      link.download = "여리패턴.png"; // 다운로드 이미지 파일 이름
+      link.click();
+      document.body.removeChild(link);
+    });
+  };
+
+  return (
+    <div
+      ref={divRef}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        maxWidth: "580px",
+      }}
+    >
+      <div
+        id="download"
+        style={{
+          width: "100%",
+          padding: isMobile ? "23px" : "40px",
+        }}
+      >
+        <div>
+          <PretendardText
+            style={{
+              color: "#111",
+              fontSize: isMobile ? "18px" : "35px",
+              fontWeight: "600",
+              lineHeight: isMobile ? "25.2px" : "49px",
+              letterSpacing: isMobile ? "-0.35px" : "-0.6px",
+              textAlign: "center",
+            }}
+          >
+            여리패턴
+            <br />
+            신체 치수 결과입니다.
+          </PretendardText>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: isMobile ? "8.66px" : "10px",
+            width: "100%",
+            marginTop: isMobile ? "17px" : "30px",
+          }}
+        >
+          <SizeBox title="이름" value={name} />
+          <SizeBox title="어깨" value={shoulder} isCm={true} />
+          <SizeBox title="가슴둘레" value={chest} isCm={true} />
+          <SizeBox title="소매길이" value={sleeve} isCm={true} />
+          <SizeBox title="허리둘레" value={waist} isCm={true} />
+          <SizeBox title="상의총장" value={top} isCm={true} />
+          <SizeBox title="하의총장" value={bottom} isCm={true} />
+        </div>
+        <div
+          style={{
+            width: "100%",
+            backgroundColor: "#f6f6f6",
+            borderRadius: "4px",
+            padding: isMobile ? "10px" : "14px 15px",
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: isMobile ? "8.66px" : "10px",
+          }}
+        >
+          <PretendardText
+            style={{
+              color: "#777",
+              fontSize: isMobile ? "12px" : "18px",
+              fontWeight: "400",
+              lineHeight: isMobile ? "18px" : "27px",
+              letterSpacing: isMobile ? "-0.35px" : "-0.6px",
+            }}
+          >
+            패턴유형
+          </PretendardText>
+          <img
+            src={`/pattern${pattern}.png`}
+            alt=""
+            style={{
+              maxWidth: isMobile ? "52px" : "100%",
+              height: isMobile ? "52px" : "auto",
+            }}
+          />
+        </div>
+        <>
+          <div style={{ width: "100%", marginTop: "40px" }}>
+            <PretendardText
+              style={{
+                color: "#111",
+                fontSize: isMobile ? "12px" : "18px",
+                fontWeight: "600",
+                lineHeight: isMobile ? "18px" : "27px",
+                letterSpacing: isMobile ? "-0.35px" : "-0.6px",
+              }}
+            >
+              상의 치수
+            </PretendardText>
+            <div style={{ display: "flex", marginTop: "15px" }}>
+              <StandardSizeBox
+                title="사이즈"
+                value={standardSize[topSize]?.size}
+              />
+              <StandardSizeBox
+                title="어깨"
+                value={standardSize[topSize]?.shoulder}
+              />
+              <StandardSizeBox
+                title="가슴 둘레"
+                value={standardSize[topSize]?.chest}
+              />
+              <StandardSizeBox
+                title="소매"
+                value={standardSize[topSize]?.sleeve}
+              />
+              <StandardSizeBox
+                title="총장"
+                value={standardSize[topSize]?.top}
+              />
+            </div>
+          </div>
+          <div style={{ width: "100%", marginTop: "40px" }}>
+            <PretendardText
+              style={{
+                color: "#111",
+                fontSize: isMobile ? "12px" : "18px",
+                fontWeight: "600",
+                lineHeight: isMobile ? "18px" : "27px",
+                letterSpacing: isMobile ? "-0.35px" : "-0.6px",
+              }}
+            >
+              하의 치수
+            </PretendardText>
+            <div style={{ display: "flex", marginTop: "15px" }}>
+              <StandardSizeBox
+                title="사이즈"
+                value={standardSize[bottomSize]?.size}
+              />
+              <StandardSizeBox
+                title="가슴 둘레"
+                value={standardSize[bottomSize]?.waist}
+              />
+              <StandardSizeBox
+                title="총장"
+                value={standardSize[bottomSize]?.bottom}
+              />
+            </div>
+          </div>
+        </>
+      </div>
+      <div
+        style={{
+          width: "100%",
+          padding: isMobile ? "0 23px" : "0 40px",
+          paddingBottom: isMobile ? "23px" : "40px",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            width: "163px",
+            padding: isMobile ? "8.66px 0" : "15px 0",
+            backgroundColor:
+              name &&
+              shoulder &&
+              chest &&
+              sleeve &&
+              waist &&
+              top &&
+              bottom &&
+              pattern
+                ? "#8E847D"
+                : "#CCCCCC",
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: "50px",
+            cursor: "pointer",
+            // marginTop: isMobile ? "17px" : submission ? "60px" : "30px",
+          }}
+          onClick={() => {
+            onClickDownloadButton();
+          }}
+        >
+          <PretendardText
+            style={{
+              color: "#fff",
+              fontSize: isMobile ? "12px" : "18px",
+              fontWeight: "500",
+              lineHeight: isMobile ? "18px" : "27px",
+              letterSpacing: isMobile ? "-0.35px" : "-0.6px",
+            }}
+          >
+            다운로드 하기
+          </PretendardText>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ResultPage;
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import React from "react";
 import { route } from "./route";
 import Formpage from "../pages/formPage";
+import ResultPage from "../pages/resultPage";
 
 const Router = () => {
   return (
     <Routes>
       {/* default */}
       <Route path={route.default} element={<Formpage />} />
+      <Route path={route.result} element={<ResultPage />} />
     </Routes>
   );
 };
